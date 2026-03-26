458a1834cb75d96899bbd29523d024c25fbcbea6
Jonas Kleineisel <jonas.kleineisel@tngtech.com>
2026-03-26 14:44:13 +0100

    export

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -5,14 +5,12 @@ auftrag_path = "./data/Auftrag_01_24-07_25.csv"
 resultat_path = "./data/Resultat_01_24-07_25.csv"
 
 auftrag_df = pd.read_csv(auftrag_path, delimiter=";")
-resultat_df = pd.read_csv(resultat_path, delimiter=";")#,nrows=1000 )
+resultat_df = pd.read_csv(resultat_path, delimiter=";")
 
 resultat = ddb.read_csv(resultat_path, delimiter=";")
 
 resultat.groupby("ANALYTX").sort("ANALYTX").size()
-
 resultat["ANALYTX"].unique().size()
-
 
 resultat = ddb.read_csv(resultat_path, delimiter=";")
 
@@ -21,8 +19,9 @@ import duckdb as ddb
 
 resultat_path = "./data/Resultat_01_24-07_25.csv"
 
-# Load the CSV → DuckDB relation
 resultat = ddb.read_csv(resultat_path, delimiter=";")
+auftrag = ddb.read_csv(auftrag_path, delimiter=";")
+
 
 sql = """
       SELECT
@@ -35,3 +34,7 @@ sql = """
 
 #  "r" is the name we expose inside the SQL string
 summary_df = resultat.query("r", sql).df()
+
+# Export summary DataFrame to CSV
+summary_path = "./data/summary_df.csv"
+summary_df.to_csv(summary_path, index=False)
